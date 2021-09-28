cbd4ea7a35c9822e0560327d6b30236d397dd2a3
JosePedroZarate <jzaratespinosa@gmail.com>
2021-09-27 22:52:37 -0500

    Closes #11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
+
 
 @Component({
   selector: 'app-home',
@@ -39,20 +41,28 @@ export class HomeComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe((res: string[]) => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+
+
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+
   }
 
   removeItem(i: number) {
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
+
   }
 
 }
